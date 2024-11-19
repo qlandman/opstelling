b7a7570e8df65e63c7549a309ed2cc6dc6ded92f
Quirijn Landman <quirijn@landmail.net>
2024-11-19 22:05:35 +0100

    Fixed bug when no Keepers selected

--- app/main.py
+++ app/main.py
@@ -44,26 +44,28 @@ async def submit_form(
         if not set(keepers).issubset(set(available)):
             warnings.append("- Een van de geselecteerde keepers is niet aanwezig")
 
-    # Create an list with the index values of keepers
-    keeper_idx = [available.index(keeper) for keeper in keepers]
+    if len(warnings) == 0:
+        # Create an list with the index values of keepers
+        keeper_idx = [available.index(keeper) for keeper in keepers]
 
-    # Create an list with the index values of available team members
-    team_idx = [Team.team_members.index(member) for member in available]
+        # Create an list with the index values of available team members
+        team_idx = [Team.team_members.index(member) for member in available]
 
-    n_team: int = len(available)  # Number of team members
-    n_tasks: int = len(Team.positions_base)  # Number of team positions
-    n_subs: int = n_team - n_tasks  # The number of subs
+        n_team: int = len(available)  # Number of team members
+        n_tasks: int = len(Team.positions_base)  # Number of team positions
+        n_subs: int = n_team - n_tasks  # The number of subs
 
-    # Determine the amount of substitutes depending on how many members are available
-    positions_sub = ["Wissel" for i in range(1, n_subs + 1)]
-    positions = Team.positions_base + positions_sub
+        # Determine the amount of substitutes depending on how many members are available
+        positions_sub = ["Wissel" for i in range(1, n_subs + 1)]
+        positions = Team.positions_base + positions_sub
 
-    matrix = generate_opstelling(n_team, Team.n_periods, team_idx, keeper_idx, n_subs)
+        matrix = generate_opstelling(
+            n_team, Team.n_periods, team_idx, keeper_idx, n_subs
+        )
 
-    team_table = create_team_table(matrix, available, positions, Team.n_parts)
-
-    # available_members = available
-    # [member for member in Team.team_members if member in available]
+        team_table: list = create_team_table(matrix, available, positions, Team.n_parts)
+    else:
+        team_table = []
 
     return templates.TemplateResponse(
         "result.html",
